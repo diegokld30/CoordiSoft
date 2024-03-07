aee47243d4f65943a9b0877891e93928f9e572e9
diegokld30 <dfercalderon@poligran.edu.co>
2024-03-07 15:15:30 -0500

    Se agrega el endpoint de la api a urls.py general

--- BackEndCoordiSoft/urls.py
+++ BackEndCoordiSoft/urls.py
@@ -42,6 +42,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router_post.urls)),
     path('areas/', include('app.areas.urls')),
+    path('api/', include('user.api.router')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
