4915ee60eb70eff2019bcb11653e3135a3cbc601
diegokld30 <dfercalderon@poligran.edu.co>
2024-03-07 14:23:15 -0500

    Se soluciona problema de visualizacion de nombre en el admin, ya las areas se pueden leer por su nombre propio y fecha de creacion y no como un objeto.

--- app/areas/serializers.py
+++ app/areas/serializers.py
@@ -4,5 +4,5 @@ from app.areas.models import Areas
 class AreasSerializer(ModelSerializer):
     class Meta:
         model = Areas
-        fields = ['nombre_area','sede', 'created_at']
-        #fields = '__all__'
+        #fields = ['nombre_area','sede', 'created_at']
+        fields = '__all__'
